aa7daf030bd069fc18429916246f9a3186629489
Simone Taeggi <startae14@gmail.com>
2021-05-17 00:49:37 +0200

    MAIN 09: render the list of songs and use semantic ui for the layout

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,10 +3,22 @@ import { connect } from 'react-redux';
 
 class SongList extends Component {
   render() {
-    console.log(this.props);
     return (
-      <div>
-        SongList
+      <div className="ui divided list">
+        {
+          this.props.songs.map(song => {
+            return (
+              <div className="item" key={song.title}>
+                <div className="right floated content">
+                  <button className="ui button primary">
+                    Select
+                  </button>
+                </div>
+                <div className="content">{song.title}</div>
+              </div>
+            );
+          })
+        }
       </div>
     );
   }
