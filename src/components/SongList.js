94904fba9338f6a7432cc9f466086aab717b1ec1
Simone Taeggi <startae14@gmail.com>
2021-05-17 19:26:14 +0200

    MAIN 10: pass the action as object in the connect of react-redux

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 class SongList extends Component {
   render() {
@@ -10,7 +11,10 @@ class SongList extends Component {
             return (
               <div className="item" key={song.title}>
                 <div className="right floated content">
-                  <button className="ui button primary">
+                  <button 
+                    className="ui button primary"
+                    onClick={() => this.props.selectSong(song)}
+                  >
                     Select
                   </button>
                 </div>
@@ -25,9 +29,12 @@ class SongList extends Component {
 }
 
 const mapStateToProps = (state) => {
+  console.log(state);
   return {
     songs: state.songs  
   };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {
+  selectSong
+})(SongList);
